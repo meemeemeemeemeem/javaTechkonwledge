6004902c04bedf8225f9bbf728df5170ed4903cb
mei <mei@>
2023-03-11 00:56:30 +0800

    add

--- src/main/java/study/FirstSample.java
+++ src/main/java/study/FirstSample.java
@@ -3,16 +3,19 @@ package study;
 
 import java.io.Console;
 import java.lang.System;
+import java.util.Date;
+import java.util.Formattable;
+import java.util.Scanner;
+
 
 /**
  * @Author mei
  * @create 2023/1/9 11:32
  */
 public class FirstSample {
-    private String temp;
+
     public static void main(String[] args) {
-        double x  = 1000/3.0;
-        System.out.printf("%8.2f",x);
+        System.out.printf("%s  %<te, %<tY","Due date:",new Date());
 
     }
 }
