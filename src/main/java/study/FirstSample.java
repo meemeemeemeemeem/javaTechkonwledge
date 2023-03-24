e3efae61abe1b13ff6e39266c3ce26525eab4539
mei <mei@puxun.com>
2023-03-24 14:35:46 +0800

    1

--- src/main/java/study/FirstSample.java
+++ src/main/java/study/FirstSample.java
@@ -1,8 +1,11 @@
 package study;
 
 
+import java.awt.*;
 import java.lang.System;
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
 
 
 /**
@@ -12,9 +15,25 @@ import java.math.BigInteger;
 public class FirstSample {
 
     public static void main(String[] args) {
-        int[] smallPrimes={2,3,5,7,11,13,};
-        System.out.println(smallPrimes);
+        Scanner in = new Scanner(System.in);
+        System.out.println("how many numbers do you need to draw");
+        int k = in.nextInt();
+        System.out.println("high number");
+        int n = in.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i]=i+1;
+        }
+        int[] result=new int[k];
+        for (int i = 0; i < result.length; i++) {
+            int r =(int)Math.random()*n;
+            result[i]=numbers[r];
+            numbers[r]=numbers[n-1];
+            n--;
+        }
+        Arrays.sort(result);
+        for (int r :result) {
+            System.out.println(r);
+        }
     }
-
-
 }
