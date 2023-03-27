1eb678643893b642ce7703466ad486b8af886474
mei <mei@puxun.com>
2023-03-27 15:38:56 +0800

    1

--- src/main/java/study/FirstSample.java
+++ src/main/java/study/FirstSample.java
@@ -1,12 +1,9 @@
 package study;
 
 
-import java.awt.*;
-import java.lang.System;
-import java.math.BigInteger;
+import java.sql.SQLOutput;
 import java.util.Arrays;
-import java.util.Scanner;
-
+import java.util.Date;
 
 /**
  * @Author mei
@@ -15,25 +12,10 @@ import java.util.Scanner;
 public class FirstSample {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("how many numbers do you need to draw");
-        int k = in.nextInt();
-        System.out.println("high number");
-        int n = in.nextInt();
-        int[] numbers = new int[n];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i]=i+1;
-        }
-        int[] result=new int[k];
-        for (int i = 0; i < result.length; i++) {
-            int r =(int)Math.random()*n;
-            result[i]=numbers[r];
-            numbers[r]=numbers[n-1];
-            n--;
-        }
-        Arrays.sort(result);
-        for (int r :result) {
-            System.out.println(r);
-        }
+        Date deadline ;
+        deadline.toString();
+
+
     }
+
 }
