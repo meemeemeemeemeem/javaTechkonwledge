cdf5e982a3d62bc9435a6b46d05ce33c5b3828fd
mei <mei@puxun.com>
2023-03-27 17:02:59 +0800

    1

--- src/main/java/org/example/entity/Son.java
+++
@@ -1,8 +0,0 @@
-package org.example.entity;
-
-/**
- * @Author mei
- * @create 2023/1/1 17:05
- */
-public class Son extends Man{
-}
--- src/main/java/study/FirstSample.java
+++ src/main/java/study/FirstSample.java
@@ -1,9 +1,10 @@
 package study;
 
 
-import java.sql.SQLOutput;
+import org.example.entity.People;
+
+import java.awt.*;
 import java.util.Arrays;
-import java.util.Date;
 
 /**
  * @Author mei
@@ -12,8 +13,26 @@ import java.util.Date;
 public class FirstSample {
 
     public static void main(String[] args) {
-        Date deadline ;
-        deadline.toString();
+        String[]i = {"123","234"};
+        String[]j = i;
+        System.out.println(Arrays.toString(i));
+        System.out.println(Arrays.toString(j));
+        System.out.println(i);
+        System.out.println(j);
+        i[0]="111";
+        System.out.println(Arrays.toString(i));
+        System.out.println(Arrays.toString(j));
+        System.out.println(i);
+        System.out.println(j);
+
+        j[0]="222";
+        System.out.println(Arrays.toString(i));
+        System.out.println(Arrays.toString(j));
+        System.out.println(i);
+        System.out.println(j);
+
+
+
 
 
     }
