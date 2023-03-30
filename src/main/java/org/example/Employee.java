43f76ab47b2cb8816de79cf2cfa8b5e7e5dd52da
mei <mei@123>
2023-03-30 19:58:05 +0800

    1

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.Objects;
 
 /**
@@ -11,36 +12,24 @@ public class Employee {
     private String name;
     private LocalDate hireDay;
     private double salary;
+    private final Date date;
+    private static int nextid;
+
 
     public Employee(String n, double salary, int year, int month, int day) {
-        name = Objects.requireNonNull(n, "the name can not be null");
+        this.name = Objects.requireNonNull(n, "the name can not be null");
         this.salary = salary;
-        hireDay = LocalDate.of(year, month, day);
+        this.hireDay = LocalDate.of(year, month, day);
+        this.date=new Date();
+        nextid++;
     }
 
-    public void raiseSalary(double byPercent) {
-        double raise = this.salary * byPercent / 100;
-        this.salary += raise;
-    }
-
-    public Employee() {
-    }
-
-    public String getName() {
-        return name;
+    public static int getNextid() {
+        return nextid;
     }
 
 
-    public LocalDate getHireDay() {
-        return hireDay;
-    }
-
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
+    public void giveGoldStar(){
+        this.date.setTime(100);
     }
 }
--- src/main/java/study/FirstSample.java
+++ src/main/java/study/FirstSample.java
@@ -8,14 +8,14 @@ import org.example.Employee;
  * @create 2023/1/9 11:32
  */
 public class FirstSample {
+    private final String name="";
+
     public static void main(String[] args)  {
 
         Employee employee = new Employee("mei",50000,2012,12,31);
-        System.out.println(employee.getName()+" "+employee.getSalary()+" "+employee.getHireDay());
-
-        double tenYearsInMilliSeconds= 10*365.25*24*60*60*1000;
-
-        System.out.println(employee.getHireDay().hashCode());
+        System.out.println(Employee.getNextid());
+        Employee employee1 = new Employee("mei",50000,2012,12,31);
+        System.out.println(Employee.getNextid());
 
     }
 }
