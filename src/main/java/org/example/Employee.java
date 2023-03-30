57790dcef14cb13c672e11fb02aefe53e9078885
mei <mei@123>
2023-03-30 16:37:29 +0800

    1

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -30,17 +30,11 @@ public class Employee {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public LocalDate getHireDay() {
         return hireDay;
     }
 
-    public void setHireDay(LocalDate hireDay) {
-        this.hireDay = hireDay;
-    }
 
     public double getSalary() {
         return salary;
--- src/main/java/study/FirstSample.java
+++ src/main/java/study/FirstSample.java
@@ -3,20 +3,19 @@ package study;
 
 import org.example.Employee;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Objects;
-
 /**
  * @Author mei
  * @create 2023/1/9 11:32
  */
 public class FirstSample {
     public static void main(String[] args)  {
+
         Employee employee = new Employee("mei",50000,2012,12,31);
-        System.out.println(employee.getName()+employee.getSalary()+employee.getHireDay());
-        employee.raiseSalary(5);
-        System.out.println(employee.getSalary());
+        System.out.println(employee.getName()+" "+employee.getSalary()+" "+employee.getHireDay());
+
+        double tenYearsInMilliSeconds= 10*365.25*24*60*60*1000;
+
+        System.out.println(employee.getHireDay().hashCode());
 
     }
 }
