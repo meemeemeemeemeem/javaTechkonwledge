3e7208f61baa2dc59981b548e68b358481c24f69
mei <mei@puxun.com>
2023-04-19 16:46:49 +0800

    1

---
+++ src/main/java/study/Manager.java
@@ -0,0 +1,20 @@
+package study;
+
+/**
+ * @author mei
+ * @since 2023/4/19 16:34
+ */
+public class Manager extends Employee {
+    private double bonus;
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year,month,day);
+    }
+    public double getSalary(){
+        double baseSalary=super.getSalary();
+        return baseSalary+this.bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+}
--- src/main/java/study/Test.java
+++ src/main/java/study/Test.java
@@ -1,95 +1,13 @@
 package study;
 
-import java.util.Random;
-
 /**
- * @Author mei
- * @create 2023/1/9 11:32
+ * @author mei
+ * @since 2023/4/19 16:34
  */
 public class Test {
     public static void main(String[] args) {
-        Student student = new Student();
-        int i = 1;
-    }
-}
+        var boss = new Manager("Carl Cracker", 80000, 1987, 12, 14);
 
-class Employee {
-    private static int nextId;
-    private int id;
-    private String name;
-    private double salary;
-
-
-    public Employee() {
-        this.name = "";
-        this.salary = 0;
     }
 
-    public Employee(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public Employee(double salary) {
-        this("Employee #" + nextId, salary);
-        nextId++;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public void raiseSalary(double byPercent) {
-        double raise = this.salary * byPercent / 100;
-        this.salary += raise;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-}
-
-
-class Student {
-
-    public static int nextId;
-
-    private int id = assignId();
-
-    static {
-        var generator = new Random();
-        nextId = generator.nextInt(10000);
-        System.out.println(nextId);
-    }
-
-    private int assignId() {
-        int r = nextId;
-        nextId++;
-        System.out.println(nextId);
-        return r;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
