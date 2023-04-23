855411d15b4bbedd20cf298bbbd62717cfb2ae63
mei <mei@puxun.com>
2023-04-23 17:22:28 +0800

    1

--- src/main/java/study/Test.java
+++ src/main/java/study/Test.java
@@ -1,14 +1,15 @@
 package study;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 /**
  * @author mei
  * @since 2023/4/19 16:34
  */
 public class Test {
     public static void main(String[] args) {
-
-
-
 
     }
 
